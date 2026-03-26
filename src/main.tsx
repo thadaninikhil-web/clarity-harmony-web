25e4244bdec21617368559705c11abfda77aeda7
thadaninikhil-web <thadaninikhil@gmail.com>
2026-03-26 16:42:13 +0530

    Update main.tsx
    Remove basename from BrowserRouter for Cloudflare Pages

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename="/clarity-harmony-web">
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 );
